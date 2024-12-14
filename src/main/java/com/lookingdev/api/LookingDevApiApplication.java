6f281e761211c9d7d357ed99cdff69f48b422aac
freshhuk <freshhuk13@gmail.com>
2024-12-14 23:21:31 +0200

    Initial commit

---
+++ src/main/java/com/lookingdev/api/LookingDevApiApplication.java
@@ -0,0 +1,13 @@
+package com.lookingdev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingDevApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookingDevApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lookingdev/api/LookingDevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookingdev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingDevApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
